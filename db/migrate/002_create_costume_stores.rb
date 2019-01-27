aa919c62fd93b6579ba44877ccbe2f059d435661
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 14:25:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,9 @@ class CostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |table|
       table.string :name
-      table.string :location 
+      table.string :location
       table.integer :number_of_costumes
       
-    end 
-  end 
+    end
+  end
 end
