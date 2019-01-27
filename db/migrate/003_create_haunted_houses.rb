7acf70bf73eeea75b70785da1f263496f4cbbb50
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 14:38:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,12 +6,12 @@ class HauntedHouses < ActiveRecord::Migration[4.2]
       table.string :name
       table.string :location
       table.string :theme
-      table.float :price 
+      table.float :price
       table.boolean :family_friendly
       table.datetime :opening_time
-      table.datetime :closing_time 
+      table.datetime :closing_time
       table.text :long_description
-    end 
+    end
   end
 
 end
