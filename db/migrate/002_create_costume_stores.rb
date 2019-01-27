888c6f4a1548b6834ba6b38f905a90090052ecb7
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-27 14:26:45 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |table|
       table.string :name
       table.string :location
-      table.integer :number_of_costumes
-      table.integer :number_of_employees
+      table.integer :num_of_costumes
+      table.integer :num_of_employees
       table.boolean :inBusiness
       table.datetime :opening_time
       table.datetime :closing_time
